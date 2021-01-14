a21aea86635bea14cb1ff19c58b233770ce312c6
Vikas2004 <S538336@NWMISSOURI.EDU>
2021-01-14 17:56:35 -0600

    contact, help and enhanced help pages update

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
+// Module Dependencies
+
 const express = require('express')
+
+// create express app
+
 const app = express()
+
+// log port 
 
 const port = process.env.PORT || 3000
 app.listen(port)
-console.log(`Running on ${port}`)
+console.log(`App is running on ${port}`)
+
 
 app.get("/",(req,res)=>{
     res.send("Hello World from Vikas's app!!")
+})
+
+app.get('/about',(req,res)=>{
+    res.send("This is a basic node express application which will also be deployed using the Heroku.")
+})
+
+app.get('/contact',(req,res)=>{
+    res.send("You have raeched the contact page of Vikas. Please reach out to me at S538336@nwmissouri.edu")
+})
+
+app.get('/help',(req,res)=>{
+    res.send("You have requested the help page! ")
+})
+
+app.get('/help/:topic',(req,res)=>{
+    res.send("login page! ")
 })
